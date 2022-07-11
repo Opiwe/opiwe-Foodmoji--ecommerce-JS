1b498b016ca08696c60b72f03340b4b0c4c5a2e3
Nkqubela Jojo <jnkqubela@gmail.com>
2022-07-11 12:23:33 +0200

    header search form active

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,5 @@
-let searchForm = document.querySelector('search-form');
+let searchForm = document.querySelector('.search-form');
 
-document.querySelector('search-btn').onclick = () => {
+document.querySelector('#search-btn').onclick = () => {
     searchForm.classList.toggle('active');
 }
