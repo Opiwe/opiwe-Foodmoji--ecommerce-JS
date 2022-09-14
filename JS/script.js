d4acaf79998aa3b95ef451c145b8bd6875ca5ab5
Nkqubela Jojo <jnkqubela@gmail.com>
2022-09-14 14:56:06 +0200

    added form login

--- JS/script.js
+++ JS/script.js
@@ -3,3 +3,9 @@ let searchForm = document.querySelector('.search-form');
 document.querySelector('#search-btn').onclick = () => {
     searchForm.classList.toggle('active');
 }
+
+let shoppingCart = document.querySelector(".shopping-cart");
+
+document.querySelector("#cart-btn").onclick = () => {
+    shoppingCart.classList.toggle("active");
+};
