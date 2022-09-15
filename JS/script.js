8d0c681b58a86b30896fb2cccafcdc8ff45de1fb
Nkqubela Jojo <jnkqubela@gmail.com>
2022-09-15 08:44:44 +0200

    added something

--- JS/script.js
+++ JS/script.js
@@ -9,3 +9,19 @@ let shoppingCart = document.querySelector(".shopping-cart");
 document.querySelector("#cart-btn").onclick = () => {
     shoppingCart.classList.toggle("active");
 };
+
+let loginForm = document.querySelector(".login-form");
+
+document.querySelector("#login-btn").onclick = () => {
+    loginForm.classList.toggle("active");
+};
+
+let navbar = document.querySelector(".navbar");
+
+document.querySelector("#menu-btn").onclick = () => {
+    navbar.classList.toggle("active");
+};
+
+window.onscroll = () => {
+    navbar.classList.remove("active");
+};
