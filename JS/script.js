807949932e12c4881d8786304f77aea2254a5301
Nkqubela Jojo <jnkqubela@gmail.com>
2022-09-16 13:50:54 +0200

    added slider swiper js

--- JS/script.js
+++ JS/script.js
@@ -40,3 +40,24 @@ window.onscroll = () => {
     loginForm.classList.remove("active");
     navbar.classList.remove("active");
 }
+
+var swiper = new Swiper(".product-slider", {
+    loop: true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 7500,
+        disableOnInteratction: false,
+    },
+    centeredSlides: true,
+    breakpoints: {
+        0: {
+        slidesPerView: 1,
+        },
+        768: {
+        slidesPerView: 2,
+        },
+        1020: {
+        slidesPerView: 3,
+        },
+    },
+});
